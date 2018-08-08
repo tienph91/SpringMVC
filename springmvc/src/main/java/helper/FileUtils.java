7cd2e2a540487bb5e47e4063dd0ae4b7a3e3510e
tienph <hoangtien.cp@gmail.com>
2018-08-08 16:46:07 +0700

    add reading content file and list files in folders

--- springmvc/src/main/java/helper/FileUtils.java
+++ springmvc/src/main/java/helper/FileUtils.java
@@ -1,17 +1,69 @@
 package helper;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileUtils {
 
-	public void readingFiles(String pathFolders) throws IOException {
+	public static void main(String[] args) {
 
-		try (Stream<Path> paths = Files.walk(Paths.get("/home/you/Desktop"))) {
-			paths.filter(Files::isRegularFile).forEach(System.out::println);
+		final File folder = new File(
+				"/media/administrator/01D42D3278CA0FF0/Ubuntu/INES/Document/Document/20_製造/DDL/table");
+
+		List<String> listFilesName = new FileUtils().readingFiles(folder);
+
+		String content = new FileUtils().concatContentInAllFiles(listFilesName);
+		
+		try {
+			PrintWriter out = new PrintWriter("filename.txt");
+			
+			out.println(content);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
 		}
+	}
+
+	public List<String> readingFiles(final File folder) {
+
+		List<String> listFileName = new ArrayList<>();
+
+		for (final File fileEntry : folder.listFiles()) {
+			if (fileEntry.isDirectory()) {
+				readingFiles(fileEntry);
+			} else {
+				listFileName.add(fileEntry.getAbsolutePath());
+			}
+		}
+
+		return listFileName;
+	}
+
+	public String concatContentInAllFiles(List<String> listFilesName) {
+		StringBuilder content = new StringBuilder();
+
+		for (String fileName : listFilesName) {
+
+			try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+
+				String line;
+				while ((line = br.readLine()) != null) {
+					content.append(line);
+					content.append("\n");
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			content.append("\n");
+		}
+
+		return content.toString();
 	}
 }
